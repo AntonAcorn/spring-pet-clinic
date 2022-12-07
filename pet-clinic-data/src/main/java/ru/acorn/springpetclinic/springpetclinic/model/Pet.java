8e29770bec8f104a627b1e6d387efb40986108c6
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-07 18:30:37 +0300

    Adding Long id and BaseEntity. closes #8

--- pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package ru.acorn.springpetclinic.springpetclinic.model;
 
 import java.time.LocalDateTime;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDateTime birthDate;
--- pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package ru.acorn.springpetclinic.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
