ff7d244e0b1dfd4eb41d7e42c2a61599886d317a
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-07 19:41:58 +0300

    Refactoring to ommon crud app. closes #9

--- pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/OwnerService.java
@@ -2,12 +2,6 @@ package ru.acorn.springpetclinic.springpetclinic.services;
 
 import ru.acorn.springpetclinic.springpetclinic.model.Owner;
 
-import java.util.Optional;
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/VetService.java
@@ -1,12 +1,7 @@
 package ru.acorn.springpetclinic.springpetclinic.services;
 
-import ru.acorn.springpetclinic.springpetclinic.model.Owner;
 import ru.acorn.springpetclinic.springpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long> {
 }
