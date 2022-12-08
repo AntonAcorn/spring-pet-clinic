98432564b4282c9cbc9a9d4415777ca6cb012568
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-08 19:27:01 +0300

    Create Pet type map Service implement #27

---
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ru.acorn.springpetclinic.springpetclinic.services;
+
+import ru.acorn.springpetclinic.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
