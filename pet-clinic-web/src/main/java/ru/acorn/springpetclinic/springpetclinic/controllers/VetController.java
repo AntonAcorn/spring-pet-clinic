1bbb8d902d4ecf24c4630b8fd74c38221fb3d5dc
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-08 13:33:15 +0300

    List all vets on index page closes #13

--- pet-clinic-web/src/main/java/ru/acorn/springpetclinic/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/acorn/springpetclinic/springpetclinic/controllers/VetController.java
@@ -1,12 +1,22 @@
 package ru.acorn.springpetclinic.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.acorn.springpetclinic.springpetclinic.services.VetService;
+import ru.acorn.springpetclinic.springpetclinic.services.map.VetServiceMap;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
