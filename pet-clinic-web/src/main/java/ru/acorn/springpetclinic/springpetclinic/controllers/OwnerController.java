4b6376a8c4e8a65cb27679772b2c2ee45ed30917
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-08 13:26:12 +0300

    List all owner on owner index page closes #14

--- pet-clinic-web/src/main/java/ru/acorn/springpetclinic/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/acorn/springpetclinic/springpetclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package ru.acorn.springpetclinic.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.acorn.springpetclinic.springpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","", "/index","index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
