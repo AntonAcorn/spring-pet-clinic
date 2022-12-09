1c95bfad3e184b9ef586cb5e46d16381eba8c82f
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-09 10:00:10 +0300

    Create specialty service closes #29

---
+++ pet-clinic-data/src/main/java/ru/acorn/springpetclinic/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package ru.acorn.springpetclinic.springpetclinic.services;
+
+import ru.acorn.springpetclinic.springpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService <Specialty, Long>{
+}
